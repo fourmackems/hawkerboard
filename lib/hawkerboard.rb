15a7c622385333a16160a101925c987e9c437763
Matt Evans <matt@thebetacollective.co.uk>
2013-05-30 13:36:29 +0100

    Updated hawkerboard.rb

--- lib/hawkerboard.rb
+++ lib/hawkerboard.rb
@@ -9,12 +9,16 @@ class Hawkerboard < Sinatra::Base
   set :views, File.join(File.dirname(__FILE__), '../views')
   set :public_folder, File.join(File.dirname(__FILE__), '../public')
   # enable :sessions
-  #Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yml'))
+  Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yml'))
 
   get '/' do
     erb :index
   end
 
+  get '/items' do
+    content_type :json
+    Item.all.to_json
+  end
 
   # for allowing a user to sign up
   post 'sign_up' do
@@ -22,14 +26,11 @@ class Hawkerboard < Sinatra::Base
     #so how do we redirect to the add a new item but within the backbone app...!? (Matt)
   end
 
-
   # for adding a new item
-  post '/add_item' do
-    Item.create(:item_name => params['item_name'], :item_price => params['item_price'], :item_description => params['item_description'], :item_tags => params['item_tags'])
+  post '/items' do
+    Item.create(JSON.parse(request.body.read.to_s))
     #so how do we redirect to a confirmation page but within the backbone app...!? (Matt)
   end
-
-
 
 
   # start the server if ruby file executed directly
