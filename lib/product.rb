58e7f36b3eaa61307277cc64838a9ac604003ecd
Matt Evans <matt@thebetacollective.co.uk>
2013-05-30 16:40:56 +0100

    deleted some bits

--- lib/product.rb
+++
@@ -1,7 +0,0 @@
-class Product
-	include Mongoid::Document
-	field :title, type: String
-	field :description, type: String
-	field :price, type: Integer
-	belongs_to :products
-end
--- lib/products.rb
+++
@@ -1,6 +0,0 @@
-class Products
-	include Mongoid::Document
-	field :product_group, type: String
-
-	has_many :posts
-end
